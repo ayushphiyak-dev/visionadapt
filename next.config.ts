d332ec4588f6dbeae6a305b1bcc682ecfc5d1bfe
Ayush phiyak <ayushphiyak.cse2025@nsec.ac.in>
2026-08-05 17:22:31 +0545

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  typedRoutes: true,
-};
-
-export default nextConfig;
